b5f988d82abc5364a71737a638fab3a04632c5aa
TheRealGramdalf <79593869+TheRealGramdalf@users.noreply.github.com>
2021-10-27 10:30:01 -0700

    Added path testing

--- main.ps1
+++ main.ps1
@@ -1,9 +1,31 @@
 "Welcome to the OverSplit CLI"
 "Here you will be guided through the process of cutting and splicing your Overdrive audiobooks into a single *.m4b file"
 "First, please enter the path to the location of your audiobooks (this will be a folder containing several folders with names like 'bd000bd9-3a9c-402c-a31a-d01a74c5f16a-425'"
+# Set the default path
 $defpath = $env:UserProfile + '\Music\My Media\OverDrive'
-$userpath = read-host "Enter the path to your audiobooks (press enter for default - default is $defpath)"
-if( $userpath -eq '' )
+# Get input from the  
+$pathin = read-host "Enter the path to your audiobooks (press enter for default - default is $defpath)"
+if( $pathin -eq '' )
 {
-    $userpath = $defpath
+    $pathin = $defpath
+}
+# Set testvar as = false so the script will do the while loop until the user gets it correct
+$testvar = "false"
+# Check if the input is a valid path, and if not, prompt the user to try again.
+while ( $testvar -eq "false" )
+{
+    if (!(test-path -path $pathin))
+    {
+        "Invalid path! Please try again."
+        $pathin = read-host "Enter the path to your audiobooks (press enter for default - default is $defpath)"
+        if( $pathin -eq '' )
+        {
+            $pathin = $defpath
+        }
+    }
+    else{
+        "Valid path! Continuing script..."
+        "Your selected path is $pathin"
+        $testvar = "true"
+    }
 }
